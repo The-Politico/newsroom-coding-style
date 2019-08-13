a19e953b5dc229f51fbb0641a117ab022198ed4f
Andrew Briz <briz.andrew@gmail.com>
2019-08-13 11:26:34 -0400

    Added react-hooks.

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 module.exports = {
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   root: true,
   env: {
-    browser: true
+    browser: true,
   },
+  plugins: [
+    'react-hooks',
+  ],
   rules: {
-    semi: ["error", "always"],
-    "comma-dangle": [
-      "error",
+    semi: ['error', 'always'],
+    'comma-dangle': [
+      'error',
       {
-        arrays: "always-multiline",
-        objects: "always-multiline",
-        imports: "never",
-        exports: "never",
-        functions: "never"
-      }
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
     ],
-    "operator-linebreak": ["error", "after"],
-    "space-before-function-paren": ["error", "never"],
-    "react/prop-types": ["skipUndeclared"]
+    'operator-linebreak': ['error', 'after'],
+    'space-before-function-paren': ['error', 'never'],
+    'react/prop-types': ['skipUndeclared'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-  extends: ["standard", "standard-react", "plugin:jsx-a11y/recommended"]
+  extends: [
+    'standard',
+    'standard-react',
+    'plugin:jsx-a11y/recommended',
+  ],
 };
